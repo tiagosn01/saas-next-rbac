833f12004b4101308577b90d10587c0e38b274a4
Tiago Silva <tiagosilva0922@gmail.com>
2024-04-25 01:11:31 -0300

    initial configs of mono-repo

---
+++ config/prettier/index.mjs
@@ -0,0 +1,22 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWith: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false
+}
+
+
+export default config
